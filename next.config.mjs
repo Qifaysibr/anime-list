5186fed717216c0c9f1518e1869765e205272e03
Qifaysibr <afim121@gmail.com>
2025-01-24 19:27:58 +0700

    setUp assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/projects/anime',
   basePath: '/projects/anime', // Menentukan basePath untuk semua routing dan asset
   images: {
     domains: ['cdn.myanimelist.net'], // Mendukung domain untuk memuat gambar eksternal
