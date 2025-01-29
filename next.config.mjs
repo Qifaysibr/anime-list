79945faa7c7de5472dfe5c316246a0963b03e1cc
Qifaysibr <afim121@gmail.com>
2025-01-29 17:28:42 +0700

    disable caching

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ['cdn.myanimelist.net'], // Mendukung domain untuk memuat gambar eksternal
   },
+  async headers() {
+    return [
+      {
+        // Apply to every page by default
+        source: '/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store' }, // Set to 'no-store' to disable caching
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig; // Export konfigurasi dengan benar
